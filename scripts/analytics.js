26a9671e82ecffa854e904ebe9439567bef4f019
Mike <michaelpeterhartmann94@gmail.com>
2023-10-13 14:18:28 -0700

    fix spelling

--- scripts/analytics.js
+++
@@ -1,6 +0,0 @@
-const incrementVisitCounter = () => {
-	const foundVisits = window.localStorage.getItem('_mphVisits');
-	const visits = (foundVisits && parseInt(foundVisits))++ || 1;
-	window.localStorage.setItem('_mphVisits', visits);
-	return visits;
-}
