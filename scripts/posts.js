f16dd4a7928e5aeda9adb99e6833e0228f343496
Mike <michaelpeterhartmann94@gmail.com>
2023-11-09 13:53:22 -0800

    adjust index of post to use mm-yy-ind format

--- scripts/posts.js
+++ scripts/posts.js
@@ -1,5 +1,5 @@
 export const posts = {
-	"001": {
+	"11-23-001": {
 		"title": "Packing for Indefinite Travel Lengths",
 		"description": "I'm going travelling, so what should I pack and how should I carry it all? I talk through my options and reasoning behind my packing plan. Be warned, it's long and rambly.",
 		"date": "01.11.23",
