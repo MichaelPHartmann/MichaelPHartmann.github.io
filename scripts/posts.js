cb964867ff2d9ec062dc7809fc4b9580c4d5b26f
Mike <michaelpeterhartmann94@gmail.com>
2023-11-09 13:43:08 -0800

    start a blog

--- scripts/posts.js
+++ scripts/posts.js
@@ -1,6 +1,6 @@
 export const posts = {
 	"001": {
-		"title": "Packing for Indefinte Travel Lengths Part 1",
+		"title": "Packing for Indefinite Travel Lengths",
 		"description": "I'm going travelling, so what should I pack and how should I carry it all? I talk through my options and reasoning behind my packing plan. Be warned, it's long and rambly.",
 		"date": "01.11.23",
 		"file": "../posts/packing-plan.html",
