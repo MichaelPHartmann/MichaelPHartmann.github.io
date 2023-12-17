a8fb6afad4080aa5a20de528be28aaf9302593a9
Mike <michaelpeterhartmann94@gmail.com>
2023-12-17 05:18:19 -0800

    add taiwan post to blog page

--- scripts/posts.js
+++ scripts/posts.js
@@ -5,5 +5,12 @@ export const posts = {
 		"date": "01.11.23",
 		"file": "../posts/packing-plan.html",
 		"tags": ['post', 'travel', 'packing']
+	},
+	"12-23-002": {
+		"title": "My First(ish) Week in Taiwan",
+		"description": "These are a few notes and observations after my first full week in Taiwan.",
+		"date": "17.12.23",
+		"file": "../posts/taiwan-first-week.html",
+		"tags": ['post', 'travel', 'taiwan']
 	}
 };
